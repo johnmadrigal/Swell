003714663134089c98ded8227acf014f70a77fa7
John Madrigal <johnmadrigal17@gmail.com>
2020-08-29 16:35:12 -0700

    store in variable

--- test/dbModel.js
+++ test/dbModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(`${process.env.MONGO_URI}`, {
+const mongoURI = process.env.MONGO_URI
+console.log('uri', mongoURI);
+mongoose.connect(mongoURI, {
   // options for the connect method to parse the URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
